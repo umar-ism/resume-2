f4beabc54fc03239730ea028de3439ccd9e7ead5
umar-ism <umarismaildjanoff@gmail.com>
2021-02-10 13:17:45 +0500

    add contact section and finish the web site

--- script.js
+++ script.js
@@ -11,3 +11,17 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 });        
+
+// typing animation script
+var typed = new Typed(".typing",{
+    strings: ["Developer", "Designer", "YouTuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
+var typed = new Typed(".typing-2",{
+    strings: ["Developer", "Designer", "YouTuber", "Blogger"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+})
